cdc222885521d7afb2b2f1057a251ed52b02134a
Mateo Olarte <mateo.olarte8@gmail.com>
2018-02-23 22:54:57 -0500

    [add] frontend all reports page

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -2,9 +2,11 @@ $(document).on("turbolinks:load", () => {
   $(".form-report__types-box").on("click", function () {
     const typeReportID = $(this).data("typereport")
 
-    $("input#report_type_report_id").attr("value", typeReportID)
+    if (!$(this).hasClass("block-click")) {
+      $("input#report_type_report_id").attr("value", typeReportID)
 
-    $(".form-report__types-box").removeClass("scale-up")
-    $(this).addClass("scale-up")
+      $(".form-report__types-box").removeClass("scale-up")
+      $(this).addClass("scale-up")
+    }
   })
 })
