7212b97a4f4e0ef785315b97047b257e6744774d
Mateo Olarte <mateo.olarte8@gmail.com>
2018-05-05 23:32:32 -0500

    [fix] change resolve for join

--- server/server.js
+++ server/server.js
@@ -234,7 +234,7 @@ app.post("/api/signup", (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
